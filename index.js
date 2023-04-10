231c8e50806fe2f4ceeddb3557d70afe791e7359
AntonMakarkin <amakarkin27@gmail.com>
2023-04-10 22:57:02 +0300

    Fixed cors

--- index.js
+++ index.js
@@ -9,7 +9,10 @@ const app = express();
 
 app.use(express.json({ limit: '30mb', extended: true }))
 app.use(express.urlencoded({ limit: '30mb', extended: true }))
-app.use(cors());
+app.use(cors({
+  credentials: true,
+  origin: ['https://memories-app-updated.netlify.app/']
+}));
 
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
